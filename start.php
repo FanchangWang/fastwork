fe89fcc2450069a721af5a428f957d2672b3c490
shaojuntan <admin@yumufeng.com>
2019-02-03 12:51:34 +0800

    增加swoole版本检测

--- start.php
+++ start.php
@@ -11,6 +11,10 @@ use fastwork\Container;
 require __DIR__ . '/vendor/autoload.php';
 
 define('APP_DEBUG', true);
+// 检测swoole版本
+if (version_compare(swoole_version(), '4.2.12', '<')) {
+    exit("\e[41mswoole version is less than 4.2.12\e[0m" . PHP_EOL);
+}
 // 应用初始化
 $app_path = __DIR__ . '/app/';
 $app = Container::get('fastwork')->setAppPath($app_path);
